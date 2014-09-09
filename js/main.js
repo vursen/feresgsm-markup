a80af66f5f93ee2f76fbe40e2dcd415c5e72af37
Sergey <pettsonchello@gmail.com>
2014-09-09 22:11:13 +0400

    Исправления плавности

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ $(document).ready(function() {
         $('.b-faq__questions__item')
             .removeClass('active')
             .find('.b-faq__answer')
-            .slideUp(300);
+            .slideUp();
 
         $this
             .parent('.b-faq__questions__item')
             .addClass('active')
             .find('.b-faq__answer')
-            .slideDown(300);
+            .slideDown();
     });
 
     $('.b-solutions__slider').bxSlider({
@@ -25,3 +25,31 @@ $(document).ready(function() {
     $('textarea, input').placeholder();
 
 });
+
+$.fn.slideUpTransition = function() {
+    return this.each(function() {
+        var $el = $(this);
+        $el.css("max-height", "0");
+        $el.addClass("height-transition-hidden");
+    });
+};
+
+$.fn.slideDownTransition = function() {
+    return this.each(function() {
+        var $el = $(this);
+        $el.removeClass("height-transition-hidden");
+
+        // temporarily make visible to get the size
+        $el.css("max-height", "none");
+        var height = $el.outerHeight();
+
+        // reset to 0 then animate with small delay
+        $el.css("max-height", "0");
+
+        setTimeout(function() {
+            $el.css({
+                "max-height": height
+            });
+        }, 1);
+    });
+};
