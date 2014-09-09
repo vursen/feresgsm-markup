f0f34d5c706d989455780fa697a60d81201c33bd
Sergey <pettsonchello@gmail.com>
2014-09-09 19:36:14 +0400

    Вроде релиз

--- js/main.js
+++ js/main.js
@@ -3,9 +3,16 @@ $(document).ready(function() {
     $('.b-faq__question').click(function() {
         var $this = $(this);
 
-        $('.b-faq__questions__item').removeClass('active');
+        $('.b-faq__questions__item')
+            .removeClass('active')
+            .find('.b-faq__answer')
+            .hide();
 
-        $this.parent('.b-faq__questions__item').addClass('active');
+        $this
+            .parent('.b-faq__questions__item')
+            .addClass('active')
+            .find('.b-faq__answer')
+            .slideDown(300);
     });
 
     $('.b-solutions__slider').bxSlider({
@@ -14,5 +21,7 @@ $(document).ready(function() {
         nextText: '',
         prevText: '',
     });
+
+    $('textarea, input').placeholder();
 
 });
